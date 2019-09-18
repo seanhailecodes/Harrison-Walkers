81974766048a5f7cd49938b777515e3455f70cea
Sean Haile <seanhaile.codes@gmail.com>
2019-09-17 22:18:52 -0600

    update pricing

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
             <div>
                 <p className="lead">
                     <div className="font">
-                    Hello neighbors, thanks for visiting our webpage. We are Harrison Walkers, Your Friendly Neighborhood Dog Walkers. Though our dog walking services are catered to the community of Harrison (apologies neighboring towns we love your dogs too), this website is designed for dog lovers of all sorts. You can utilize the search bar below to find photos of some of your favorite dogs! The search function can be a bit finicky, but you can type in as few as the first letter or two of your favorite breed to have a look. Please come back soon and get your daily doggy dose of goodness.
+                    Hello neighbors, thanks for visiting our webpage. We are Harrison Walkers, Your Friendly Neighborhood Dog Walkers. Though our dog walking services are catered to the community of Harrison (apologies neighboring towns we love your dogs too), this website is designed for dog lovers of all sorts. You can utilize the search bar below to find photos of some of your favorite dogs! The search function can be a bit finicky, but you can type in as few as the first letter or two of your favorite breed to have a look. Please come back soon and get your daily dose of doggy goodness.
                     </div>
             </p>
             </div>
