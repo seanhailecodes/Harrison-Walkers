4b9a8679d277d2f35a7840afff86b6ef13ec00f4
Sean Haile <seanhaile.codes@gmail.com>
2019-09-17 22:14:21 -0600

    updated pricing

--- src/SampleB.js
+++ src/SampleB.js
@@ -8,21 +8,28 @@ export default class SampleB extends Component {
                     
                     <div className="font">
                     <br />
-                 Dog walking services are currently offerred to residents of Urby, Steel Works, Vermella, Cobalt and Station/330. It is because of our amazing teams' love of dogs, and close proximity to our select service area that we are able to offer <span className="bolda">the best dog walking services in Harrison at the best prices. </span> 
+                 Dog walking services are currently offerred to residents of Harrison. It is because of our truly amazing teams' love of dogs, and close proximity to our select service area that we are able to offer <span className="bolda">the best dog walking services in Harrison at the best prices. </span> 
                     <br />
                     <br />
                     <div className="col-sm-4 offset-sm-4">
                     <span className="bold"> Our Upfront Pricing</span>
                     <br />
                     •Base walks are 30 minutes in duration
+                    <br /> 
+                    •1 mid-day walk per weekday
+
                     <br />
-                    ∞ under 15lbs @ $50 per week (1 walk per weekday)<br />
-                    ∞ over 15lbs @ $65 per week  (1 walk per weekday)<br />
-                    ∞ available only to dogs, and owners, who play nice<br/> 
+                    •under 15lbs @ $50 per week<br />
+                    •over 15lbs @ $65 per week<br />
+                    
+                    <br/>
+                    <span className="bold">Individual walks </span>
+                    <br />
+
                     ∞ under 15lbs @ $14 per day <br />
                     ∞ over 15lbs @ $17 per day <br />
                     <br />
-                    All walks include GPS tracking providing you with instant time stamped updates on exactly where your beloved pet was walked. Perhaps over by TaKorea (which we love) or towards Red Bull Stadium, where ever they go you will know step for step.
+                    All walks include GPS tracking providing you with instant time stamped updates on exactly where your beloved pet was walked. Perhaps over by TaKorea (which we love) or towards Red Bull Stadium, wherever they go you will know step for step.
                     <br />
                     </div>
                    
